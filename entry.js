c8820703d3219b5226a8522cd6507b6403752232
Jo Liss <joliss42@gmail.com>
2017-03-02 20:20:27 +0000

    Use ES6 class for Entry

--- entry.js
+++ entry.js
@@ -1,19 +1,20 @@
 'use strict';
 
-function Entry(relativePath, basePath, mode, size, mtime) {
-  this.mode = mode;
+class Entry {
+  constructor(relativePath, basePath, mode, size, mtime) {
+    this.mode = mode;
 
-  this.relativePath = relativePath;
-  this.basePath = basePath;
-  this.size = size;
-  this.mtime = mtime;
+    this.relativePath = relativePath;
+    this.basePath = basePath;
+    this.size = size;
+    this.mtime = mtime;
 
-  this.linkDir = false;
+    this.linkDir = false;
+  }
+
+  isDirectory() {
+    return (this.mode & 61440) === 16384;
+  }
 }
-
-
-Entry.prototype.isDirectory = function isDirectory() {
-  return (this.mode & 61440) === 16384;
-};
 
 module.exports = Entry;
